3fba289c8c8e3a2f7e208ad2f5de1b1fa3146dc9
Nokvix <igor.gass2003@mail.ru>
2024-10-28 22:58:22 +0500

    Добавь модели и локализуй

--- blogicum/blog/admin.py
+++ blogicum/blog/admin.py
@@ -1,3 +1,7 @@
-# from django.contrib import admin
+from django.contrib import admin
+from .models import Category, Location, Post
 
-# Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Location)
+admin.site.register(Post)
